aa96b0ea6afe27d7a39f451d31e7f7da9944b4ce
Albrtx Zurc <Albrtx Zurc>
2016-10-25 17:09:03 -0400

    first commit

---
+++ app/src/main/java/com/evolutiondso/www/preffragment/AppPreferences.java
@@ -0,0 +1,32 @@
+package com.evolutiondso.www.preffragment;
+
+import android.app.FragmentManager;import android.preference.PreferenceFragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppPreferences extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_note_layout);
+
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+        android.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        SettingsFragment settingsFragment = new SettingsFragment();
+        fragmentTransaction.add(android.R.id.content, settingsFragment, "SETTINGS_FRAGMENT");
+        fragmentTransaction.commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.app_preferences);
+        }
+    }
+
+}
